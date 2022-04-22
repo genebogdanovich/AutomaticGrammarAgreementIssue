e78b727d62f85e696bb716942a15fef4a7490da1
Gene Bogdanovich <genebogdanovich@gmail.com>
2022-04-22 11:49:58 +0300

    Initial Commit

---
+++ AutomaticGrammarAgreementIssue/AutomaticGrammarAgreementIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomaticGrammarAgreementIssueApp.swift
+//  AutomaticGrammarAgreementIssue
+//
+//  Created by Gene Bogdanovich on 22.04.22.
+//
+
+import SwiftUI
+
+@main
+struct AutomaticGrammarAgreementIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutomaticGrammarAgreementIssue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AutomaticGrammarAgreementIssue
+//
+//  Created by Gene Bogdanovich on 22.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
