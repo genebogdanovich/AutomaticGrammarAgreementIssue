ce40de18871c5445997bc9dcb1a371d46d42a460
Gene Bogdanovich <genebogdanovich@gmail.com>
2022-04-22 13:44:04 +0300

    Make sample project.

--- AutomaticGrammarAgreementIssue/ContentView.swift
+++ AutomaticGrammarAgreementIssue/ContentView.swift
@@ -8,9 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    /// This value mimics what **StoreKit 2** returns for `Product.SubscriptionPeriod.Unit` in its `localizedDescription` on iOS 15.4.
+    let unit = "Week"
+    
+    @State var value = 1
+    
+    var key: LocalizedStringKey {
+        "Free trial: \(value) \(unit)"
+    }
+    
+    var plainStringKey: String {
+        "Free trial: \(value) \(unit)"
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section {
+                // Works fine without string interpolation.
+                Text(key)
+                    .foregroundColor(.green)
+                
+                // `NSLocalizedString` doesn't work and it shouldn't as per its docs.
+                Text("\(NSLocalizedString(plainStringKey, comment: "")) free")
+                    .foregroundColor(.red)
+                
+                // Doesn't see the string.
+                Text("\(String(localized: String.LocalizationValue(stringLiteral: plainStringKey))) free")
+                    .foregroundColor(.red)
+                
+                // This way also doesn't see the string, which is strange, because it should be just like the following way, which does see the string.
+                Text(String(localized: String.LocalizationValue.init(plainStringKey)))
+                    .foregroundColor(.red)
+                
+                // Sees the string (only if I pass literal, not the plainStringKey property), but doesn't apply automatic grammar agreement.
+                Text("\(String(localized: String.LocalizationValue("Free trial: \(value) \(unit)"))) free")
+                    .foregroundColor(.red)
+                
+                
+                // MARK: Bad solution:
+                /*
+                 - Doesn't work with Dynamic Type properly
+                 - You have to approximate horizontal spacing
+                 */
+                
+                HStack(spacing: 3) {
+                    Text("Free trial: \(value) \(unit)")
+                        .textCase(.lowercase)
+                    Text("free")
+                }
+                .foregroundColor(.orange)
+            }
+            
+            Section {
+                Stepper(value: $value, in: 1...2) {
+                    Text("Value: \(value)")
+                }
+            }
+        }
     }
 }
 
